13d6453e64f0da40ddb3c58f9a9b0b71411f0421
nonydev <karldjansi123@gmail.com>
2026-05-02 14:29:26 +0000

    added tab labels

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,37 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="about" />
-      <Tabs.Screen name="profile" />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "blue",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "Home",
+          tabBarLabel: "Home",
+          tabBarIcon: () => <Ionicons name="home" size={20} />,
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          headerTitle: "About",
+          tabBarLabel: "About",
+          tabBarIcon: () => <Ionicons name="information-circle" size={20} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerTitle: "Profile",
+          tabBarLabel: "Profile",
+          tabBarIcon: () => <Ionicons name="accessibility" size={20} />,
+        }}
+      />
     </Tabs>
   );
 }
