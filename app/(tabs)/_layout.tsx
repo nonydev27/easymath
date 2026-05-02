88b97d5ad83e1cdd1c558d6b666785a6b7d4f5b0
nonydev <karldjansi123@gmail.com>
2026-05-02 20:47:37 +0000

    added-status-bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,37 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "blue",
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          headerTitle: "Home",
-          tabBarLabel: "Home",
-          tabBarIcon: ({ focused, color }) => (
-            <Ionicons name={focused ? "home" : "home-outline"} size={20} />
-          ),
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "blue",
         }}
-      />
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            headerTitle: "Home",
+            tabBarLabel: "Home",
+            tabBarIcon: ({ focused, color }) => (
+              <Ionicons name={focused ? "home" : "home-outline"} size={20} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="lessons"
-        options={{
-          headerTitle: "Lessons",
-          tabBarLabel: "Lessons",
-          tabBarIcon: ({ focused, color }) => (
-            <Ionicons name={focused ? "book" : "book-outline"} size={20} />
-          ),
-        }}
-      />
-      {/* <Tabs.Screen
+        <Tabs.Screen
+          name="lessons"
+          options={{
+            headerTitle: "Lessons",
+            tabBarLabel: "Lessons",
+            tabBarIcon: ({ focused, color }) => (
+              <Ionicons name={focused ? "book" : "book-outline"} size={20} />
+            ),
+          }}
+        />
+        {/* <Tabs.Screen
         name="about"
         options={{
           headerTitle: "About",
@@ -44,19 +46,22 @@ export default function TabLayout() {
           ),
         }}
       /> */}
-      <Tabs.Screen
-        name="profile"
-        options={{
-          headerTitle: "Profile",
-          tabBarLabel: "Profile",
-          tabBarIcon: ({ focused, color }) => (
-            <Ionicons
-              name={focused ? "accessibility" : "accessibility-outline"}
-              size={20}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="profile"
+          options={{
+            headerTitle: "Profile",
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ focused, color }) => (
+              <Ionicons
+                name={focused ? "accessibility" : "accessibility-outline"}
+                size={20}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+
+      <StatusBar style="dark" />
+    </>
   );
 }
